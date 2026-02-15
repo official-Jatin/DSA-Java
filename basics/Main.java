cd2fb2e08276e8ee1afbf4a432edc62a2845192a
Jatin <work.jatinx@gmail.com>
2026-02-15 21:38:33 +0530

    dsa

---
+++ basics/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main (String args[]) {
+        int a = 20 ;
+        int b = 25;
+        int c = 69;
+
+        if ( a > b ) {
+            
+        }
+    }
+}
